9d0e7de140d494b899c2a7d2c54dafca30ce9895
Raoyu <q360584748@qq.com>
2018-07-04 16:15:47 +0800

    yyl-health 提交表单

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,53 +1,29 @@
 <template>
-	<el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
-		<el-form-item label="活动名称">
-			<el-input v-model="form.name"></el-input>
-		</el-form-item>
-		<el-form-item label="活动区域">
-			<el-select v-model="form.region" placeholder="请选择活动区域">
-				<el-option label="区域一" value="shanghai"></el-option>
-				<el-option label="区域二" value="beijing"></el-option>
+	<el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:300px">
+		<el-form-item label="小程序名">
+			<el-select v-model="form.appName" placeholder="请选择小程序名" style="display:block;width:200px;">
+				<el-option label="yylhealth" value="yylhealth"></el-option>
+				
 			</el-select>
 		</el-form-item>
-		<el-form-item label="活动时间">
-			<el-col :span="11">
-				<el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-			</el-col>
-			<el-col class="line" :span="2">-</el-col>
-			<el-col :span="11">
-				<el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-			</el-col>
+        <el-form-item label="设备">
+			<el-select v-model="form.device" placeholder="请选择设备" style="display:block;width:200px;">
+				<el-option label="YR01BP8621510309255051" value="YR01BP8621510309255051"></el-option>
+				<el-option label="YR01BP8621510309255052" value="YR01BP8621510309255052"></el-option>
+			</el-select>
 		</el-form-item>
-		<el-form-item label="即时配送">
-			<el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
-		</el-form-item>
-		<el-form-item label="活动性质">
-			<el-checkbox-group v-model="form.type">
-				<el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-				<el-checkbox label="地推活动" name="type"></el-checkbox>
-				<el-checkbox label="线下主题活动" name="type"></el-checkbox>
-				<el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-			</el-checkbox-group>
-		</el-form-item>
-		<el-form-item label="特殊资源">
-			<el-radio-group v-model="form.resource">
-				<el-radio label="线上品牌商赞助"></el-radio>
-				<el-radio label="线下场地免费"></el-radio>
-			</el-radio-group>
-		</el-form-item>
-		<el-form-item label="活动形式">
-			<el-input type="textarea" v-model="form.desc"></el-input>
-		</el-form-item>
-		<el-form-item>
-			<el-button type="primary">立即创建</el-button>
-			<el-button @click.native.prevent>取消</el-button>
-		</el-form-item>
+        <el-upload action="http://localhost:3000/upload" name="thumbnail" :on-success="handleSuccess" :on-error="handleError" :beforeUpload="beforeAvatarUpload">
+        <el-button size="small" type="primary">点击上传Excel文件</el-button>
+        <div slot="tip" class="el-upload__tip">只能上传xlsx文件</div>
+        </el-upload>
 	</el-form>
 </template>
 
 <script>
-	export default {
-		data() {
+import XLSX from 'xlsx'
+
+export default {
+    data() {
 			return {
 				form: {
 					name: '',
@@ -61,11 +37,33 @@
 				}
 			}
 		},
-		methods: {
-			onSubmit() {
+  methods:{
+    onSubmit() {
 				console.log('submit!');
-			}
-		}
-	}
+			},  
+    handleSuccess() {
+      console.log("success")
+      this.$message.success('上传成功');
+    },
+    handleError(err) {
+      this.$message.error(`上传失败: ${err}`);
+    },
+    beforeAvatarUpload (file) {
+      const extension = file.name.split('.')[1] === 'xls'
+      const extension2 = file.name.split('.')[1] === 'xlsx'
+      const isLt10M = file.size / 1024 / 1024 < 10
+      if (!extension && !extension2 && !extension3 && !extension4) {
+        console.log('上传模板只能是 xls、xlsx 格式!')
+      }
+      if (!isLt10M) {
+        console.log('上传模板大小不能超过 10MB!')
+      }
+      return extension || extension2  && isLt10M
+    }
+  },
+}
 
 </script>
+
+<style scoped>
+</style>
